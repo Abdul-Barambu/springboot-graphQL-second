b79028126fa4b1390d46f8bb6f94dcb544d50a1a
Abdul-Barambu <abdulkadirbarambu1@gmail.com>
2024-03-30 14:37:02 +0100

    first commit

---
+++ springboot-graphql-second/src/main/java/com/abdul/springbootgraphqlsecond/SpringbootGraphqlSecondApplication.java
@@ -0,0 +1,13 @@
+package com.abdul.springbootgraphqlsecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraphqlSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGraphqlSecondApplication.class, args);
+	}
+
+}
---
+++ springboot-graphql-second/src/test/java/com/abdul/springbootgraphqlsecond/SpringbootGraphqlSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdul.springbootgraphqlsecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGraphqlSecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
